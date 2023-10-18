9500509942bd175ce158ba9b815a036620952526
Seanneskie <seannecanete32@gmail.com>
2023-10-18 20:03:17 +0800

    Update inventory.sql

--- inventory.sql
+++ inventory.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3308
--- Generation Time: May 23, 2023 at 01:29 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: Oct 18, 2023 at 01:27 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,8 +20,60 @@ SET time_zone = "+00:00";
 --
 -- Database: `inventory`
 --
-CREATE DATABASE IF NOT EXISTS `inventory` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `inventory`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `Category_ID` int(11) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `flavor`
+--
+
+CREATE TABLE `flavor` (
+  `flavor_ID` int(11) NOT NULL,
+  `flavor` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `product_num` int(11) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `product_stock` varchar(255) NOT NULL,
+  `exp_date` datetime NOT NULL,
+  `man_date` datetime NOT NULL,
+  `Category_ID` int(11) NOT NULL,
+  `flavor_ID` int(11) NOT NULL,
+  `supplier_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `Supplier_ID` int(11) NOT NULL,
+  `supplier` varchar(255) NOT NULL,
+  `contact_num` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -29,7 +81,6 @@ USE `inventory`;
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `first_name` varchar(30) NOT NULL,
@@ -38,7 +89,7 @@ CREATE TABLE `user` (
   `password` varchar(200) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user`
@@ -55,6 +106,34 @@ INSERT INTO `user` (`id`, `first_name`, `last_name`, `email`, `password`, `creat
 --
 
 --
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`Category_ID`);
+
+--
+-- Indexes for table `flavor`
+--
+ALTER TABLE `flavor`
+  ADD PRIMARY KEY (`flavor_ID`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_num`),
+  ADD UNIQUE KEY `Category_ID` (`Category_ID`,`flavor_ID`,`supplier_ID`),
+  ADD KEY `Category_ID_2` (`Category_ID`,`flavor_ID`,`supplier_ID`),
+  ADD KEY `flavor_ID` (`flavor_ID`),
+  ADD KEY `product_ibfk_3` (`supplier_ID`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`Supplier_ID`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -65,10 +144,40 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `flavor`
+--
+ALTER TABLE `flavor`
+  MODIFY `flavor_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `product_num` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `supplier`
+--
+ALTER TABLE `supplier`
+  MODIFY `Supplier_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`flavor_ID`) REFERENCES `flavor` (`flavor_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `product_ibfk_2` FOREIGN KEY (`Category_ID`) REFERENCES `category` (`Category_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `product_ibfk_3` FOREIGN KEY (`supplier_ID`) REFERENCES `supplier` (`Supplier_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
